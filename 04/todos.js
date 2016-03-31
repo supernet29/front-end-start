a367715f6b1e0efc0687b854acc7b597566330f7
WoochanKim <woochankim@gmail.com>
2016-03-31 16:20:32 +0900

    Week 04 : i need to complete it

---
+++ 04/todos.js
@@ -0,0 +1,21 @@
+//start...
+
+function Task(taskName){
+	this.taskName = taskName;
+	this.checked = false;	
+}
+
+function makeTaskElement(task){
+	var taskElement = document.createElement('li');
+
+	//inner elements
+	var deleteButton = document.createElement('button');
+	var checkBox = document.createElement('input');
+	var title = document.createElement('span');
+
+	//set elements
+	checkBox.
+
+	taskElement.appendChild(
+
+}
