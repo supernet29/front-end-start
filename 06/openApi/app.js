080bf5e37306353d44d60ba25cc43d0a30914d35
WoochanKim <woochankim200@gmail.com>
2016-04-07 22:23:26 +0900

    OpenAPI : script is done

--- 06/openApi/app.js
+++ 06/openApi/app.js
@@ -2,18 +2,61 @@ var apiKey = '42d11779e0cbae4694ffbe909dd331ba'
 
 var resultTemplate = document.getElementById('resultTemplate').innerHTML;
 var searchBox = document.getElementById('searchBox');
+var resultListElement = document.querySelectorAll('.resultList', searchBox)[0];
+var queryInputElement = document.getElementById('queryInput');
+var searchButtonElement = document.getElementById('searchButton');
+var moreButtonElement = document.getElementById('moreButton');
 
-function getApiUrl(query) {
-	var apiUrl = 'https://apis.daum.net/search/web?apikey=' + apiKey + '&q=' + query + '&output=json';
+var currentQuery = '';
+var currentViewPage = 1;
+var maxViewPage = 3;
+
+
+function getApiUrl(query, page) {
+	var apiUrl = 'https://apis.daum.net/search/web?apikey=' + apiKey + '&q=' + query + '&pageno=' + page + '&output=json';
 	return apiUrl;
 }
 
 
-var data = {
-	title: "Hello",
-	link: "http://naver.com",
-	description: "this is a hello world program"
-};
+function render(dataList) {
+	var resultHTML = "";
+	for(var i = 0; i < dataList.length; i++) {
+		var data = dataList[i];
+		resultHTML += tmpl(resultTemplate, data);
+	}
+	resultListElement.innerHTML += resultHTML;
+}
 
+function onSearch() {
+	window.resultListElement.innerHTML = "";
 
+	var query = queryInputElement.value;
+	var apiUrl = getApiUrl(query, 1);
+	var dataRenderer = function(data) {
+		render(data.channel.item);
+	};
 
+	currentQuery = query;
+	currentViewPage = 1;
+
+	if(query == '')
+		return;
+
+	getJSON(apiUrl, dataRenderer);
+}
+
+function onMore() {
+	if(currentViewPage >= maxViewPage){
+		return;
+	}
+
+	currentViewPage++;
+	var apiUrl = getApiUrl(currentQuery, currentViewPage);
+	var dataRenderer = function(data) {
+		render(data.channel.item);
+	};
+	getJSON(apiUrl, dataRenderer);
+}
+
+searchButtonElement.addEventListener('click', onSearch);
+moreButtonElement.addEventListener('click', onMore);
